07d5b7d7481fa0fc3d602bcf3b1d8bc24f3beee3
vietanh <vietanhhd2608@gmail.com>
2025-04-12 15:25:23 +0700

    first commit

--- src/main/java/com/datn/datn_vanh/Config/SecurityConfig.java
+++ src/main/java/com/datn/datn_vanh/Config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -41,5 +42,9 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> null;  //ẩn password mặc định
     }
 }
